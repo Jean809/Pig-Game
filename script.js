f6ca8d5d20f5c270578fe60c92f9a2c407b34ee0
Jean809 <jean_carlosrosario709@outlook.com>
2022-07-21 22:24:58 -0400

    was able to change active player by adding RollButton inside RollButton

--- script.js
+++ script.js
@@ -8,30 +8,55 @@
 //We have to make the "roll dice" button roll the dice ----> COMPLETED
 //1.)
 
-function addsNumber(dice) {
-  dice += dice;
-}
+let currentScore0 = document.getElementById("current--0");
+let overallScore0 = document.getElementById("score--0");
+const player0 = document.querySelector(" player--0");
 
-document.querySelector(".btn--roll").addEventListener("click", function () {
-  const dice = Math.trunc(Math.random() * 6) + 1;
+//let dice = Math.trunc(Math.random() * 6) + 1; //
+let diceIMG = document.querySelector(".dice");
+const rollButton = document.querySelector(".btn--roll");
+const holdButton = document.querySelector(".btn--hold");
 
+let currentScore1 = document.getElementById("current--1");
+let overallScore1 = document.getElementById("score--1");
+
+diceIMG.style.display = "none";
+
+rollButton.addEventListener("click", function () {
+  let dice = Math.trunc(Math.random() * 6 + 1);
+  diceIMG.style.display = "block";
   if (dice === 1) {
-    document.querySelector(".dice").src = "dice-1.png";
+    diceIMG.src = "dice-1.png";
   } else if (dice === 2) {
-    document.querySelector(".dice").src = "dice-2.png";
+    diceIMG.src = "dice-2.png";
   } else if (dice === 3) {
-    document.querySelector(".dice").src = "dice-3.png";
+    diceIMG.src = "dice-3.png";
   } else if (dice === 4) {
-    document.querySelector(".dice").src = "dice-4.png";
+    diceIMG.src = "dice-4.png";
   } else if (dice === 5) {
-    document.querySelector(".dice").src = "dice-5.png";
+    diceIMG.src = "dice-5.png";
   } else if (dice === 6) {
-    document.querySelector(".dice").src = "dice-6.png";
+    diceIMG.src = "dice-6.png";
   }
-  document.getElementById("current--0").textContent =
-    Number(document.getElementById("current--0").textContent) + dice;
+  currentScore0.textContent = Number(currentScore0.textContent) + dice;
+  if (dice === 1) {
+    currentScore0.textContent = 0;
+    rollButton.addEventListener("click", function () {
+      currentScore0.textContent = 0;
+      currentScore1.textContent = Number(currentScore1.textContent) + dice;
+    });
+  }
 });
 
-document.querySelector(".currentLabel");
+//We have to keep adding the dice value into "current" value ---> COMPLETE
 
-//We have to keep adding the dice value into "current" value
+//We have to make the "HOLD" btn store the value into player's overall score// --- COMPLETED
+//We have to reset the current score each time we click the "HOLD" button// ---> COMPLETED
+//We have to switch to player 2 after "HOLD" btn is pressed// PLAYER 2 ---> SEMI-COMPLETE (NEED TO DEVIDE THE PROBLEM MORE)
+//For every time that random number is generated, we need to display a dice image representing it PLAYER 2
+
+holdButton.addEventListener("click", function () {
+  overallScore0.textContent =
+    Number(overallScore0.textContent) + Number(currentScore0.textContent);
+  currentScore0.textContent = 0;
+});
